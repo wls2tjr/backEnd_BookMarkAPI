201656c3ae08905fb4aff39e1fcaa10ef0071fd8
wls2tjr <wls2tjr@naver.com>
2025-02-19 14:09:12 +0900

    first commit

---
+++ src/main/java/com/bookmarker/api/service/BookmarkService.java
@@ -0,0 +1,27 @@
+package com.bookmarker.api.service;
+
+import com.bookmarker.api.domain.Bookmark;
+import com.bookmarker.api.domain.BookmarkRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class BookmarkService {
+    private final BookmarkRepository repository;
+
+    //Constructor Injection
+//    public BookmarkService(BookmarkRepository repository) {
+//        this.repository = repository;
+//    }
+
+    @Transactional(readOnly = true)
+    public List<Bookmark> getBookmarks() {
+        return repository.findAll();
+    }
+    
+}
