3149be0da79ae52103059044366f8e46b446b7c6
wls2tjr <wls2tjr@naver.com>
2025-02-19 14:45:54 +0900

    page commit

--- src/main/java/com/bookmarker/api/service/BookmarkService.java
+++ src/main/java/com/bookmarker/api/service/BookmarkService.java
@@ -3,6 +3,9 @@ package com.bookmarker.api.service;
 import com.bookmarker.api.domain.Bookmark;
 import com.bookmarker.api.domain.BookmarkRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,8 +23,11 @@ public class BookmarkService {
 //    }
 
     @Transactional(readOnly = true)
-    public List<Bookmark> getBookmarks() {
-        return repository.findAll();
+    public List<Bookmark> getBookmarks(Integer page) {
+        //JPA 의 페이지 언어가 0부터 시작하기때문
+        int pageNo = page < 1 ? 0 : page - 1;
+        Pageable pageable = PageRequest.of(pageNo, 10, Sort.Direction.DESC, "id");
+        return repository.findAll(pageable).getContent();
     }
     
 }
